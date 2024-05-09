e85349340262ad38e6b9b8b38a4cffcbc3cb59ee
hyper <hyperskill@hyperskill.org>
2024-05-09 16:51:58 +0300

    Register models in admin.py

--- landscapedesign/main/admin.py
+++ landscapedesign/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Project, Service, Feedback
+
+admin.site.register(Project)
+admin.site.register(Service)
+admin.site.register(Feedback)
