d2ef8ffb0a2ecc234faf83fefcc6cbcf0e91078f
hyper <hyperskill@hyperskill.org>
2024-05-09 18:35:50 +0300

    Make admin panel support GalleryImage model

--- landscapedesign/main/admin.py
+++ landscapedesign/main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project, Service, Feedback
+from .models import Project, Service, Feedback, GalleryImage
 
 admin.site.register(Project)
 admin.site.register(Service)
 admin.site.register(Feedback)
+admin.site.register(GalleryImage)
